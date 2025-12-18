68b17b5de0211ba8199872cf33fabe313b537b08
Marko <marko@bugdigger.com>
2025-12-18 18:28:42 +0100

    simplyfing the CustomExecutableConfigurationType class

--- src/main/kotlin/com/github/bbugdigger/runcustomexecutables/CustomExecutableConfigurationType.kt
+++ src/main/kotlin/com/github/bbugdigger/runcustomexecutables/CustomExecutableConfigurationType.kt
@@ -1,53 +1,46 @@
 package com.github.bbugdigger.runcustomexecutables
 
-import com.intellij.execution.configurations.ConfigurationFactory
-import com.intellij.execution.configurations.ConfigurationType
+import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.execution.configurations.SimpleConfigurationType
 import com.intellij.icons.AllIcons
-import javax.swing.Icon
+import com.intellij.openapi.components.BaseState
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.NotNullLazyValue
 
 /**
  * This is the entry point for the run configuration:
  * https://plugins.jetbrains.com/docs/intellij/run-configurations.html#configurationtype
  *
- * It appears in the "Add New Run Configuration" dialog in IntelliJ
+ * It appears in the "Add New Run Configuration" dialog in IntelliJ.
+ * 
+ * Uses SimpleConfigurationType since we only have a single configuration factory.
+ * SimpleConfigurationType acts as both the configuration type AND the factory,
+ * eliminating the need for a separate factory class.
  */
-class CustomExecutableConfigurationType : ConfigurationType {
+class CustomExecutableConfigurationType : SimpleConfigurationType(
+    "CustomExecutableRunConfiguration",
+    "Custom Executable",
+    "Run a custom executable",
+    NotNullLazyValue.createValue { AllIcons.RunConfigurations.Application }
+) {
 
     /**
-     * Returns the display name of the configuration type. This is used, for example, to represent the configuration type in the run
-     * configurations tree, and also as the name of the action used to create the configuration.
+     * Creates a new template run configuration within the context of the specified project.
+     * This method is called once for each project to create the run configuration template.
      *
-     * @return the display name of the configuration type.
+     * @param project the project in which the run configuration will be used
+     * @return a new run configuration instance
      */
-    override fun getDisplayName(): String = "Custom Executable"
+    override fun createTemplateConfiguration(project: Project): RunConfiguration {
+        return CustomExecutableRunConfiguration(project, this, "Custom Executable")
+    }
 
     /**
-     * Returns the description of the configuration type. You may return the same text as the display name of the configuration type.
+     * Returns the options class used to store the run configuration settings.
      *
-     * @return the description of the configuration type.
+     * @return the options class
      */
-    override fun getConfigurationTypeDescription(): String = "Run a custom executable"
-
-    /**
-     * Returns the 16x16 icon used to represent the configuration type.
-     *
-     * @return the icon
-     */
-    override fun getIcon(): Icon = AllIcons.RunConfigurations.Application
-
-    /**
-     * The ID of the configuration type. Should be camel-cased without dashes, underscores, spaces and quotation marks.
-     * The ID is used to store run configuration settings in a project or workspace file and
-     * must not change between plugin versions.
-     */
-    override fun getId(): String = "CustomExecutableRunConfiguration"
-
-    /**
-     * Returns the configuration factories used by this configuration type. Normally, each configuration type provides just a single factory.
-     * You can return multiple factories if your configurations can be created in multiple variants (for example, local and remote for an
-     * application server).
-     *
-     * @return the run configuration factories.
-     */
-    override fun getConfigurationFactories(): Array<ConfigurationFactory> = arrayOf(CustomExecutableConfigurationFactory(this))
+    override fun getOptionsClass(): Class<out BaseState> {
+        return CustomExecutableRunConfigurationOptions::class.java
+    }
 }
